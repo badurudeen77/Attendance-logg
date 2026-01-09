fb1f1031879ab1d77d64ca2093cee7495470bf17
badurudeen77 <badurudeens406@gmail.com>
2026-01-10 00:16:23 +0530

    Emptying repository

--- server/routes/studentCardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const studentCardController = require("../controllers/studentCardController");
-
-router.post("/issue", studentCardController.issueCard);
-router.get("/:studentId", studentCardController.getCardByStudentId);
-router.patch("/status/:studentId", studentCardController.updateCardStatus);
-
-module.exports = router;
